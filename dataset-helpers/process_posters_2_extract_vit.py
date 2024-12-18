b95a771b1614f431c3b4413f137d2cd285ba2a9f
Jose Tagarro <chema92@gmail.com>
2024-12-18 23:11:52 +0100

    Fixed issue with format of extracted ViT features. Renamed for consistency.

--- dataset-helpers/process_posters_2_extract_vit.py
+++ dataset-helpers/process_posters_2_extract_vit.py
@@ -4,8 +4,12 @@ from torchvision import transforms, models
 from PIL import Image
 import numpy as np
 
+# Define device (use GPU if available)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
 # Load pre-trained ViT model from torchvision
-vit_model = models.vit_l_16(weights=models.ViT_L_16_Weights.DEFAULT)
+vit_model = models.vit_l_16(weights=models.ViT_L_16_Weights.DEFAULT).to(device)
 
 # Define image transformations
 transform = transforms.Compose([
@@ -24,6 +28,7 @@ movie_ids = []
 
 # Process each image
 for file_name in os.listdir(image_folder):
+    #if file_name.endswith(".jpg"):
     if file_name.endswith(".jpg"):
         print(f"Processing {file_name}")
         # Extract MovieLens ID from the file name
@@ -33,16 +38,17 @@ for file_name in os.listdir(image_folder):
         # Load and preprocess the image
         image_path = os.path.join(image_folder, file_name)
         image = Image.open(image_path).convert("RGB")
-        image_tensor = transform(image).unsqueeze(0)  # Add batch dimension
+        image_tensor = transform(image).unsqueeze(0).to(device)  # Add batch dimension
 
         # Extract features from the CLS token
         with torch.no_grad():
             feats = vit_model._process_input(image_tensor)
-            batch_cls = vit_model.class_token.expand(image_tensor.shape[0], -1, -1)
+            batch_cls = vit_model.class_token.expand(feats.shape[0], -1, -1)
             feats = torch.cat([batch_cls, feats], dim=1)
             feats = vit_model.encoder(feats)
-            feats = feats[:, 0]
-            features.append(feats)
+            feats = feats[:, 0][0]
+            #feats = torch.flatten(feats, start_dim=1)  # Flatten the features
+            features.append(feats.cpu().numpy())
 
 # Convert lists to NumPy arrays
 features_array = np.array(features)
